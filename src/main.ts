1d4fdfa77449cdd8431a32b89501dba2684ec09b
Ayoub Amer <ayouub.ameur@gmail.com>
2024-10-03 17:45:53 +0100

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // swaagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Blog Platform API')
+    .setDescription('API for managing blog posts, search, and analytics')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(5000);
 }
 bootstrap();
